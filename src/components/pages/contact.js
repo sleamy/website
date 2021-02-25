a13b7b70cc9782c31d2ea7d524b503bfe318aca4
Sleamy <stleamy@gmail.com>
2021-02-25 20:38:59 +1000

    added contact form

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -4,7 +4,17 @@ import contactCSS from "./contact.module.scss"
 export default function Contact() {
     return (
         <div id="contact" className={contactCSS.container}>
-            <h1>Contact Me</h1>
+            <h1>Contact me</h1>
+            <p></p>
+            <form className={contactCSS.form}>
+                <div className={contactCSS.upper}>
+                    <input type="text" name="name" placeholder="Name"/>
+                    <input type="email" name="email" placeholder="Email"/>
+                </div>
+                <input type="text" name="subject" placeholder="Subject"/>
+                <textarea placeholder="Message"></textarea>
+                <input type="submit" rows="10" text="Send" />         
+            </form>
         </div>
     )
 }
