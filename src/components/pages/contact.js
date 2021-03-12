ed9c6c63e6fe136f763e2fe247a6499cc0ef5474
Sleamy <stleamy@gmail.com>
2021-03-12 11:51:24 +1000

    Netlify Form support

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -2,11 +2,13 @@ import React from "react"
 import contactCSS from "./contact.module.scss"
 
 export default function Contact() {
+
     return (
         <div id="contact" className={contactCSS.container}>
             <h1>Contact me</h1>
             <p></p>
-            <form className={contactCSS.form}>
+            <form className={contactCSS.form} name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                 <div className={contactCSS.upper}>
                     <label for="name"></label><input type="text" name="name" id="name" placeholder="Name"/>
                     <label for="email"></label><input type="email" name="email" id="email" placeholder="Email"/>
