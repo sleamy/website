e2070d0278223f698808c0e11fc764573df2a13a
Sleamy <stleamy@gmail.com>
2021-08-10 21:00:13 +1000

    Added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,20 @@ module.exports = {
     siteUrl: `https://www.sleamy.dev`,
   },
   plugins: [
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          // You can add multiple tracking ids and a pageview event will be fired for all of them.
+          trackingIds: [
+            "G-KSZ3Y5DCV0"
+          ],
+          // This object is used for configuration specific to this plugin
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: false,
+          },
+        },
+      },
     `gatsby-plugin-sass`, 
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-advanced-sitemap`,
